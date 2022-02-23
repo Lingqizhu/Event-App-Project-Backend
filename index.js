212804d393b55a767dacdfbb2ecd76bc8d19ad27
Lingqizhu <lingqizhu1980@gmail.com>
2022-02-23 15:11:05 +0000

    register added

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /// importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
-const { v4: uuidv4} = require('uuid');
-const { Event } = require('./models/eventSchema');
-const { User } = require('./models/userSchema');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+const { Event } = require("./models/eventSchema");
+const { User } = require("./models/userSchema");
+const bcrypt = require("bcryptjs");
 
-mongoose.connect("mongodb+srv://Lingqizhu:Peggy1980122@cluster0.k9dxo.mongodb.net/Event?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://Lingqizhu:Peggy1980122@cluster0.k9dxo.mongodb.net/Event?retryWrites=true&w=majority"
+);
 
 // defining the Express app
 const app = express();
@@ -25,35 +28,51 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.post('/auth', async (req,res) => {
-  const user = await User.findOne({userName: req.body.userName})
-  if(!user){
+
+
+app.post("/register", async (req, res) => {
+  const newPassword = await bcrypt.hash(req.body.password, 10);
+  const user = await User.create({
+    userName: req.body.userName,
+    password: newPassword,
+  });
+  await user.save();
+  res.send({ status: "ok" });
+});
+
+app.post("/auth", async (req, res) => {
+  const user = await User.findOne({ userName: req.body.userName });
+  if (!user) {
     return res.sendStatus(401);
   }
-  if(req.body.password !== user.password){
+  /* if (req.body.password !== user.password) {
     return res.sendStatus(403);
+  } */
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (isPasswordValid) {
+    user.token = uuidv4();
+    await user.save();
+    res.send({ token: user.token });
+  } else {
+    return res.sendStatus(404);
   }
-  user.token = uuidv4()
-  await user.save();
+});
 
-  res.send({token: user.token})
-})
+app.use(async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const user = await User.findOne({ token: authHeader });
 
-
-app.use( async (req,res,next) => {
-  const authHeader = req.headers['authorization'];
-  const user = await User.findOne({token: authHeader})
-
-  if(user){
+  if (user) {
     next();
   } else {
     res.sendStatus(403);
   }
-
-} )
-
+});
 
 // defining CRUD operations
 /* app.get('/', async (req, res) => {
@@ -63,50 +82,53 @@ app.use( async (req,res,next) => {
 // exports.index = function (req, res) {
 //     Book.find().then((books) => res.send(books));
 //   };
+app.get('/userlist',function(req,res){
+  User.find().then((users) => res.send(users))
+});
 
-app.get('/', function (req, res){
-    Event.find().then((events)=> res.send(events))
-})
-
+app.get("/", function (req, res) {
+  Event.find().then((events) => res.send(events));
+});
 
 //search by location
-app.get('/:location', async(req, res)=>{
-    res.send(await Event.find({location: req.params.location}))
-})
-
-app.get('/name/:name', async(req, res)=>{
-    res.send(await Event.find({name: req.params.name}))
-})
-
-
+app.get("/location/:location", async (req, res) => {
+  res.send(await Event.find({ location: req.params.location }));
+});
+/* app.get('/location/:location', async(req, res)=>{
+  await Event.find({location: req.params.location})
+  res.send({ message: 'location found.' })
+}) */
+app.get("/name/:name", async (req, res) => {
+  res.send(await Event.find({ name: req.params.name }));
+});
 
 // create new event
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   const newEvent = req.body;
- const event = new Event(newEvent);
+  const event = new Event(newEvent);
   await event.save();
-  res.send({ message: 'New event inserted.' });
+  res.send({ message: "New event inserted." });
 });
 
 // delete event
-app.delete('/:id', async (req, res) => {
-  await Event.deleteOne({ _id: ObjectId(req.params.id) })
-  res.send({ message: 'Event removed.' });
+app.delete("/:id", async (req, res) => {
+  await Event.deleteOne({ _id: ObjectId(req.params.id) });
+  res.send({ message: "Event removed." });
 });
 
 // update event
-app.put('/:id', async (req, res) => {
-  await Event.findOneAndUpdate({ _id: ObjectId(req.params.id)}, req.body )
-  res.send({ message: 'Event updated.' });
+app.put("/:id", async (req, res) => {
+  await Event.findOneAndUpdate({ _id: ObjectId(req.params.id) }, req.body);
+  res.send({ message: "Event updated." });
 });
 
 // starting the server
 app.listen(3001, () => {
-  console.log('listening on port 3001');
+  console.log("listening on port 3001");
 });
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-  console.log("Database connected!")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback() {
+  console.log("Database connected!");
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -3,6 +3,8 @@ const userSchema = mongoose.Schema({
     userName: String,
     password: String,
     token: String
-})
+},
+{ collection: 'user-data' }
+)
 
-module.exports.User = mongoose.model('User',userSchema,'User')
+module.exports.User = mongoose.model('User',userSchema)
